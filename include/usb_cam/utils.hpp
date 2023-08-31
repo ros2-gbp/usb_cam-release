582edc92f0df82b9f90b364037d991ad7811e4a6
Evan Flynn <evanflynn.msu@gmail.com>
2023-08-30 20:21:43 -0700

    Rebase from 'debian/rolling/usb_cam'

--- include/usb_cam/utils.hpp
+++ include/usb_cam/utils.hpp
@@ -50,10 +50,14 @@ namespace utils
 using usb_cam::formats::pixel_format_base;
 
 
+/// @brief Read more on IO methods here: https://lwn.net/Articles/240667/
 typedef enum
 {
+  /// @brief read method copies the video frame between user and kernal space
   IO_METHOD_READ,
+  /// @brief memory mapped buffers allocated in kernel space
   IO_METHOD_MMAP,
+  /// @brief memory buffers allocated in the user space
   IO_METHOD_USERPTR,
   IO_METHOD_UNKNOWN,
 } io_method_t;
@@ -65,10 +69,10 @@ struct buffer
 };
 
 
-/// @brief Get epoch time shift
+/// @brief Get epoch time shift in microseconds
 /// @details Run this at start of process to calculate epoch time shift
 /// @ref https://stackoverflow.com/questions/10266451/where-does-v4l2-buffer-timestamp-value-starts-counting
-inline time_t get_epoch_time_shift()
+inline time_t get_epoch_time_shift_us()
 {
   struct timeval epoch_time;
   struct timespec monotonic_time;
@@ -76,20 +80,35 @@ inline time_t get_epoch_time_shift()
   gettimeofday(&epoch_time, NULL);
   clock_gettime(CLOCK_MONOTONIC, &monotonic_time);
 
-  const int64_t uptime_ms =
-    monotonic_time.tv_sec * 1000 + static_cast<int64_t>(
-    std::round(monotonic_time.tv_nsec / 1000000.0));
-  const int64_t epoch_ms =
-    epoch_time.tv_sec * 1000 + static_cast<int64_t>(
-    std::round(epoch_time.tv_usec / 1000.0));
+  const int64_t uptime_us =
+    monotonic_time.tv_sec * 1000000 + static_cast<int64_t>(
+    std::round(monotonic_time.tv_nsec / 1000.0));
+  const int64_t epoch_us =
+    epoch_time.tv_sec * 1000000 + epoch_time.tv_usec / 1000.0;
 
-  return static_cast<time_t>((epoch_ms - uptime_ms) / 1000);
+  return static_cast<time_t>(epoch_us - uptime_us);
 }
 
-
-inline int xioctl(int fd, int request, void * arg)
+/// @brief Calculate image timestamp from buffer time and epoch time shift.
+/// In this, the buffer time is first converted into microseconds before the epoch time shift,
+/// which is to be given in microseconds is added to it. Afterwards it is split into seconds
+/// and nanoseconds for the image timestamp.
+inline timespec calc_img_timestamp(const timeval & buffer_time, const time_t & epoch_time_shift_us)
 {
-  int r;
+  timespec img_timestamp;
+
+  int64_t buffer_time_us = (buffer_time.tv_sec * 1000000) + buffer_time.tv_usec;
+  buffer_time_us += epoch_time_shift_us;
+
+  img_timestamp.tv_sec = (buffer_time_us / 1000000);
+  img_timestamp.tv_nsec = (buffer_time_us % 1000000) * 1000;
+
+  return img_timestamp;
+}
+
+inline int xioctl(int fd, uint64_t request, void * arg)
+{
+  int r = 0;
 
   do {
     r = ioctl(fd, request, arg);
